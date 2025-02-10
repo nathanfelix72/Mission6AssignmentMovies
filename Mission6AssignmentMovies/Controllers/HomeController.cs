3e9dadf338673de3bf2e885e4c43a854f1b57958
Nathan Felix <nathan.felix@gmail.com>
2025-02-10 13:50:29 -0700

    Add project files.

---
+++ Mission6AssignmentMovies/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission6AssignmentMovies.Models;
+
+namespace Mission6AssignmentMovies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext _context;
+
+        public HomeController(MovieContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetToKnow()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EnterMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnterMovie(Movie response)
+        {
+            _context.Movies.Add(response); // Add record to the database
+            _context.SaveChanges();
+
+            return View("Confirmation", response);
+        }
+    }
+}
---
+++ Mission6AssignmentMovies/Models/MovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission6AssignmentMovies.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base (options)
+        {
+
+        }
+        
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
