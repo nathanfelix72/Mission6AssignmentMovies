f68dc146caa1f5ddd811c8ca2a22256c151e346a
Nathan Felix <nathan.felix@gmail.com>
2025-02-18 15:34:16 -0700

    Ready for submission

---
+++ Mission6AssignmentMovies/Migrations/20250218223016_nulls6.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission6AssignmentMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class nulls6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Mission6AssignmentMovies/Models/Movie.cs
+++ Mission6AssignmentMovies/Models/Movie.cs
@@ -12,21 +12,28 @@ namespace Mission6AssignmentMovies.Models
 
         [ForeignKey("CategoryId")]
         public int? CategoryId { get; set; }
-        public Category? Category { get; set; }
 
+        public Category? Category { get; set; }
 
         [Required(ErrorMessage = "You must enter a title")]
         public string Title { get; set; }
+
         [Required]
         [Range(1888, int.MaxValue, ErrorMessage = "Year must be after 1888.")]
         public int Year { get; set; }
+
         public string? Director { get; set; }
+
         public string? Rating { get; set; }
+
         [Required(ErrorMessage = "You must enter if the movie is edited or not")]
         public bool Edited { get; set; }
+
         public string? LentTo { get; set; }
+
         [Required(ErrorMessage = "You must enter if the movie has been copied to Plex")]
-        public bool? CopiedToPlex { get; set; }
+        public bool CopiedToPlex { get; set; }
+
         [MaxLength(25)]
         public string? Notes { get; set; }
     }
